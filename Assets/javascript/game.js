286d9c345e33de1b55dfdd85ad762313cbe1b767
Maverick <embrymaverick@gmail.com>
2018-02-21 20:35:31 -0700

    fixes

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,10 +1,9 @@
 // Set the variables to start a foundation..
 var wordList = ["hansolo", "lukeskywalker", "yoda", "darthvader", "chewbacca", "c3po", "rey", "jangofett"];
 
-var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var chosenWord = "";
+var guess = "";
 var letterInChosenWord = [];
-var Blanks = 0;
+var numBlanks = 0;
 var blanksAndSuccesses = [];
 var wrongGuesses = [];
 
@@ -13,55 +12,42 @@ var lossCounter = 1;
 var numGuesses = 15;
 
 function startGame() {
-    /*
-    1. computer chooses a word from word list
-    2. computer breaks down that random word as letters and replace them with
-    underscores _
-    3. add those underscores to the HTML to display to the player
-    4. numguesses always equals 15, and blankandsuccess is an empty array, 
-    and wronggueses is empty as well.
-    */
+    
     wrongGuesses = [];
 
     numGuesses = 15;
     blanksAndSuccesses = [];
 
 
-    chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
-    lettersInChosenWord = chosenWord.split("");
-    Blanks = lettersInChosenWord.length;
-    
+    guess = wordList[Math.floor(Math.random() * wordList.length)];
+    lettersInChosenWord = guess.split("");
+    numBlanks = lettersInChosenWord.length;
 
-    for (var i = 0; i < Blanks; i++) {
+
+    for (var i = 0; i < numBlanks; i++) {
         blanksAndSuccesses.push("_");
     }
-    console.log(blanksAndSuccesses);
+
     document.getElementById('word-blank').innerHTML = blanksAndSuccesses.join(" ");
-    document.getElementById('guesses-left').innerHTML = "Guesses Left: " + numGuesses;
+    document.getElementById('guesses-left').innerHTML = numGuesses;
 
 }
-startGame();
-
-
-
-
-
 
 function checkLetters(letter) {
-
+    
 
     var letterInWord = false;
 
-    for (var i = 0; i < Blanks; i++) {
-        if (chosenWord[i] === letter) {
+    for (var i = 0; i < numBlanks; i++) {
+        if (guess[i] === letter) {
             letterInWord = true;
 
         }
     }
 
     if (letterInWord) {
-        for (i = 0; i < Blanks; i++) {
-            if (chosenWord[i] === letter) {
+        for (i = 0; i < numBlanks; i++) {
+            if (guess[i] === letter) {
                 blanksAndSuccesses[i] = letter;
 
             }
@@ -72,36 +58,33 @@ function checkLetters(letter) {
         wrongGuesses.push(letter)
     }
 
-
+    
 
 
 }
 
 
-function complete() {
-
+function roundComplete() {
+   
 
     document.getElementById('word-blank').innerHTML = blanksAndSuccesses.join(" ");
-    document.getElementById('guesses-left').innerHTML = "Guesses Left: " + numGuesses;
-    document.getElementById('wrong-guesses').innerHTML = "Wrong Guesses: " + wrongGuesses.join(" ");
+    document.getElementById('guesses-left').innerHTML = numGuesses;
+    document.getElementById('wrong-guesses').innerHTML = wrongGuesses.join(" ");
 
 
-
+   
 
     if (lettersInChosenWord.join(" ") === blanksAndSuccesses.join(" ")) {
         winCounter++;
-        alert("Winner Winner Chicken Dinner!" );
-        document.getElementById('win-counter').innerHTML = "You have won: " + winCounter + " time(s)";
+        alert("Winner Winner Chicken Dinner!");
+        document.getElementById('win-counter').innerHTML = winCounter;
         startGame();
     } else if (numGuesses === 0) {
         document.getElementById('loss-counter').innerHTML = lossCounter++;
         document.getElementById('wrong-guesses').innerHTML = "";
-        alert("you ran out of guesses!");
+        alert("Too bad so sad!");
         startGame();
     }
-    if (winCounter
-
-    
 
 
 
@@ -109,11 +92,11 @@ function complete() {
 }
 startGame();
 document.onkeyup = function (event) {
-
+   
     var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
 
     checkLetters(letterGuessed)
-    complete();
+    roundComplete();
 
 
 }
